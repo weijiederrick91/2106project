1b42f08ab6d85c433c56bd062ca37761af739ff0
Wei Jie Derrick <weijiederrick@Weis-MacBook-Pro.local>
2017-02-18 20:46:11 +0800

    Source Code

---
+++ ICT2016-W4-LAB-Explore-California-Start/ExploreCalifornia/Models/Booking.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExploreCalifornia.Models
+{
+    public class Booking
+    {
+
+        public int BookingID { get; set; }
+        public int TourID { get; set; }
+        public string TourName { get; set; }
+        public string ClientID { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime DepartureDate { get; set; }
+        [Required]
+        public int NumberofPeople { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        [Required]
+        public string ContactNo { get; set; }
+        public string SpecialRequest { get; set; }
+
+    }
+
+}
