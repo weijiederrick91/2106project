36cb0a7d359616d88b201de0714f05be033b6b5e
muhammadbna <muhammad.noor_2015@sit.singaporetech.edu.sg>
2017-04-03 18:56:44 +0800

    Admin is able to vote!

--- Team11_2106Project/DomainModel/Admin.cs
+++ Team11_2106Project/DomainModel/Admin.cs
@@ -7,17 +7,20 @@ using Team11_2106Project.ViewModel;
  */
 namespace Team11_2106Project.DomainModel
 {
-    public class Admin : Voter, ILogIn<Admin>
+    public class Admin : ILogIn<Admin>, IVote
     {
 
         // local variables
         private int adminID { get; set; }
+        private bool hasVoted { get; set; }
+        private static Admin currentAdmin;
 
         // adminGateway
         private IDataGateway<AdminViewModel> dataGatewayAdmin = new DataGateway<AdminViewModel>();
+        private IDataGateway<CandidateViewModel> dataGatewayCandidate = new DataGateway<CandidateViewModel>();
 
         // valiate Admin's login
-        public new bool Login(string email, string password)
+        public bool Login(string email, string password)
         {
             // iterate through every admin in the table
             foreach(AdminViewModel admin in dataGatewayAdmin.SelectAll())
@@ -25,7 +28,9 @@ namespace Team11_2106Project.DomainModel
                 // if email and password matches an Admin found in the table, admin's login is valid!
                 if (admin.Email.Equals(email) && admin.Password.Equals(password))
                 {
-                    adminID = admin.AdminID;
+                    currentAdmin = new Admin();
+                    currentAdmin.hasVoted = admin.HasVoted;
+                    currentAdmin.adminID = admin.AdminID;
                     return true;
                 }
             }
@@ -36,7 +41,27 @@ namespace Team11_2106Project.DomainModel
 
         public int getID()
         {
-            return adminID;
+            return currentAdmin.adminID;
+        }
+
+        public void Vote(int candidateID, int currentUserID)
+        {
+            // capture currentAdmin, set HasVoted to true and save it to DB
+            AdminViewModel currentUser = dataGatewayAdmin.SelectByID(currentUserID);
+            currentUser.HasVoted = true;
+            dataGatewayAdmin.Update(currentUser);
+
+            currentAdmin.hasVoted = true;   // save it in runtime class
+
+            // capture Candidate to be voted, increment TotalVotes and save it to DB
+            CandidateViewModel candidate = dataGatewayCandidate.SelectByID(candidateID);
+            candidate.TotalVotes++;
+            dataGatewayCandidate.Update(candidate);
+        }
+
+        public bool hasCurrentUserVoted()
+        {
+            return currentAdmin.hasVoted;
         }
     }
 }
